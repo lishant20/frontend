a253fce363caa1d342b9e64b73990015ed9e713b
Lishant <lishantkarki@gmail.com>
2025-01-10 18:52:11 +0545

    create 404 using react-router

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -6,6 +6,7 @@ import RootLayout from './layout/RootLayout'
 import ContactLayout from './layout/ContactLayout'
 import ContactInfo from './components/ContactInfo'
 import ContactForm from './components/ContactForm'
+import NotFound from './components/NotFound'
 
 function App() {
 
@@ -18,7 +19,8 @@ function App() {
         <Route path='contact' element={<ContactLayout />}>
           <Route path='info' element={<ContactInfo />} />
           <Route path='form' element={<ContactForm />} />
-        </Route> 
+        </Route>
+        <Route path='*' element={<NotFound />} /> 
       </Route>
     )
   )
