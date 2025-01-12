8d091e013f9d6e85fe4f34dd56d5564bb3b235cd
Lishant <lishantkarki@gmail.com>
2025-01-12 18:29:00 +0545

    create 404 page and router Loader to fetch and load data from local api

--- ReactRouterDom/src/App.jsx
+++ ReactRouterDom/src/App.jsx
@@ -7,6 +7,9 @@ import RootLayout from "./layout/RootLayout"
 import ContactLayout from "./layout/ContactLayout"
 import ContactInfo from "./components/ContactInfo"
 import ContactForm from "./components/ContactForm"
+import NotFound from './components/NotFound'
+import JobsLayout from './layout/JobsLayout'
+import Jobs, { jobsLoader } from "./pages/Jobs"
 
 function App() {
 
@@ -20,6 +23,10 @@ function App() {
           <Route path='info' element={<ContactInfo />} />
           <Route path='form' element={<ContactForm />} />
         </Route>
+        <Route path='jobs' element={<JobsLayout />}>
+          <Route index element={<Jobs />} loader={jobsLoader}/>
+        </Route>
+        <Route path='*' element={<NotFound />} />        
       </Route>
     )
   )
---
+++ ReactRouterDom/src/pages/Jobs.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+
+function Jobs() {
+
+  const jobsData = useLoaderData();
+  return(
+    <div className="jobs">
+      {jobsData.map((job) => {
+        return <Link to={job.id.toString()} key={job.id}>
+                  <h4>{job.title}</h4>
+                  <p>{job.location}</p>
+              </Link>
+      })}
+    </div>
+  )
+}
+
+export default Jobs
+
+export const jobsLoader = async ()=> {
+  const res = await fetch("http://localhost:5000/jobs");
+  if(!res.ok) {
+    throw Error("Could not found the job list");
+  }
+  return res.json();
+}
