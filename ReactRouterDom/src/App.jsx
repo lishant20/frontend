566d77eaa03a6989729494e0fe53699b72b1fe0e
Lishant <lishantkarki@gmail.com>
2025-01-12 16:29:20 +0545

    create Router using createReactBrowser(new method)

--- ReactRouterDom/src/App.jsx
+++ ReactRouterDom/src/App.jsx
@@ -1,24 +1,25 @@
 import React from "react"
-import Navbar from "./components/Navbar"
-import { Route,Routes } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Route,RouterProvider} from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Products from "./pages/Products"
 import Contact from "./pages/Contact"
+import RootLayout from "./layout/RootLayout"
 
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path='products' element={<Products />}/>
+        <Route path='about' element={<About />}/>
+        <Route path='contact' element={<Contact />}/>
+      </Route>
+    )
+  )
   return (
-    <div>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/products' element={<Products />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/contact' element={<Contact />}/>
-        </Routes>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
